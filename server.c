9e82a389caa66add4c99ca3f6b5367b2e282630a
Alita Mohiam <amohiam@oa-j5.msk.21-school.ru>
2021-09-21 15:31:35 +0300

    removed globals

--- server.c
+++ server.c
@@ -6,17 +6,18 @@
 /*   By: amohiam <amohiam@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/05 15:34:33 by amohiam           #+#    #+#             */
-/*   Updated: 2021/09/21 15:17:37 by amohiam          ###   ########.fr       */
+/*   Updated: 2021/09/21 15:28:27 by amohiam          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static int	symbol;
-static int	bit_count;
+
 
 void	get_bits(int bit)
 {
+	static int	symbol;
+	static int	bit_count;
 	symbol += ((bit & 1) << bit_count);
 	bit_count++;
 	if (bit_count == 7)
@@ -31,8 +32,6 @@ void	get_bits(int bit)
 
 int	main(void)
 {
-	symbol = 0;
-	bit_count = 0;
 	write(1, "PID: ", 5);
 	ft_putnbr(getpid());
 	write(1, "\n", 1);
